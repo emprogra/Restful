ea08359bbd50ff8dd933641b14bbdaf17e92da5f
Emily <a2017110728@estudiantes.upsa.edu.bo>
2021-02-27 12:06:05 -0400

    Ejercicio Completo

---
+++ Generador de identidad/Identidad/Model/Identity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identidad.Model
+{
+    public class Identity
+    {
+        public int IdentityID { get; set; }
+        public string Name { get; set; }
+        public string Photo { get; set; }
+        public string Sex { get; set; }
+
+    }
+}
